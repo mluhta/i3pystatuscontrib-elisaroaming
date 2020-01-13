16b8ff001d436719f9db76de720dd74d9b91be9d
Mika <mika@email>
2020-01-13 07:44:00 +0200

    Initial commit

---
+++ i3pystatuscontrib/elisaroaming/ElisaRoaming.py
@@ -0,0 +1,120 @@
+from i3pystatus import IntervalModule, formatp
+from i3pystatus.core.util import require, internet
+import requests
+import urllib
+
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0"
+API_BASE = "https://verkkoasiointi.elisa.fi/rest"
+
+class ElisaRoaming(IntervalModule):
+    """
+    Fetches used roaming quota information from OmaElisa.
+
+    Requires PyPI packages `requests`.
+
+    .. rubric:: Available formatters
+
+    * {used} amount of quota used in gigabytes
+    * {total} amount of total quota in gigabytes
+    * {free} amount of data left in gigabytes
+    * {used_percentage} percentage of data used
+
+    """
+    interval = 60
+
+    settings = (
+        "format",
+        "color",
+        ("color_warning", "color when data usage over warning_percentage threshold"),
+        ("warning_percentage", "percentage threshold for data usage, 0.0 - 1"),
+        ("email", "OmaElisa email/username"),
+        ("password", "OmaElisa password"),
+        ("number", "target phone number")
+    )
+
+    format = "ROAM: {used}Gb/{total}Gb"
+    color = "#FFFFFF"
+    color_warning = "#ff0000"
+
+    # TODO: use keyring instead
+    email = "" # OmaElisa username/email
+    password = "" # OmaElisa password
+    number = "1234567" # Target phone number
+
+    warning_percentage = 0.8
+
+    @require(internet)
+    def get_roaming_info(self):
+        try:
+            session = requests.Session()
+            session.headers.update({"User-Agent": USER_AGENT})
+            session.headers.update({"Referer": "https://verkkoasiointi.elisa.fi/"})
+
+            # Get auth code for login
+            res = session.get("{api}/public/login/password-auth-code/{email}".format(
+                api=API_BASE, email=urllib.parse.quote(self.email)))
+            res.raise_for_status() 
+            auth_code = res.json()["code"]
+
+            # Login, get SSO token
+            res = session.post(
+                    "https://id.elisa.fi/sso/login?client_id=ipa&language=fi",
+                    json={
+                        "accountId": self.email,
+                        "authCode": auth_code,
+                        "password": self.password
+                    })
+            res.raise_for_status()
+            token = res.json()["token"]
+
+            # Login with the token
+            res = session.post(
+                    "{api}/public/login".format(api=API_BASE),
+                    json={"token": token})
+            res.raise_for_status()
+
+            # Get roaming usage
+            res = session.get(
+                    "{api}/mobile/{number}/roaming-package-usage".format(
+                        api=API_BASE, number=self.number))
+            res.raise_for_status()
+            data = res.json()
+
+            # TODO: calc data left
+            return {
+                "used": data["usedInGb"],
+                "total": data["totalInGb"]
+            }
+
+        except ConnectionResetError:
+            return None
+        except requests.exceptions.ConnectionError:
+            return None
+
+    def disable(self):
+        self.output = None
+
+    def run(self):
+        usage = self.get_roaming_info()
+        if not usage:
+            return self.disable()
+
+        used_percentage = float(usage["used"])/float(usage["total"])
+        free = float(usage["total"]) - float(usage["used"])
+
+        output_color = self.color
+        if used_percentage > self.warning_percentage:
+            output_color = self.color_warning
+
+        formatting = {
+            "used": usage["used"],
+            "total": usage["total"],
+            "free": free,
+            "used_percentage": used_percentage
+        }
+
+        self.output = {
+            "full_text": formatp(self.format, **formatting).strip(),
+            "color": output_color
+        }
+
---
+++ i3pystatuscontrib/elisaroaming/__init__.py
@@ -0,0 +1,1 @@
+from .ElisaRoaming import ElisaRoaming
